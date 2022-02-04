4c2170b0934f51e051ed43027fd056bf56bc1ec7
Scott Everett <burningcupidxxx@gmail.com>
2022-02-03 20:35:53 -0800

    create seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,27 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DEC(6,2);
-    deptartment_id INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(name)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
+    FOREIGN KEY(role_id)
+    REFERENCES role(id),
     manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES employee(title),
-    -- FOREIGN KEY (manager_id)
-    -- REFERENCES
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const inquirer = require("inquirer");
 const express = require("express");
+const mysql = require("mysql2");
 
-const manageEmployeeData = () => {
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const menuPrompt = () => {
     inquirer.prompt(
         [{
             type: "list",
@@ -9,7 +15,29 @@ const manageEmployeeData = () => {
             message: "What would you like to do?",
             choices: ["View all deptartments", "Add department", "View all roles", "Add role", "View all employees", "Add employee"]
         }]
-    )
+    ).then((userResponse) => {
+        if (userResponse.options === "View all departments") {
+            console.log("view all departments");
+        } else if(userResponse.options === "Add department") {
+            console.log("Add department");
+        } else if(userResponse.options === "Add role") {
+            console.log("Add role");
+        } else if(userResponse.options === "View all employees") {
+            console.log("View all employees");
+        } else if(userResponse.options === "Add employee") {
+            console.log("Add employee");
+        }
+        menuPrompt();
+    })
 }
 
-manageEmployeeData();
+
+
+menuPrompt();
+   
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
