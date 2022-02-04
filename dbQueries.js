cc05f58a490418100423826307e96af4245b54e7
Scott Everett <burningcupidxxx@gmail.com>
2022-02-03 21:52:45 -0800

    modularize functions for each option and import to server.js

---
+++ dbQueries.js
@@ -0,0 +1,30 @@
+const showDepartments = () => {
+    console.log("testing for show departments");
+};
+
+const addDepartment = () => {
+    console.log("testing for add department");
+};
+
+const showRoles = () => {
+    console.log("testing for show roles");
+};
+
+const addRole = () => {
+    console.log("testing for add role");
+};
+
+const showEmployees = () => {
+    console.log("testing for show employees");
+};
+
+const addEmployee = () => {
+    console.log("testing for add employee")
+};
+
+const updateEmployee = () => {
+    console.log("testing for update employee")
+};
+
+module.exports = { showDepartments, addDepartment, showRoles, addRole, showEmployees, addEmployee, updateEmployee }
+
--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const inquirer = require("inquirer");
 const express = require("express");
 const mysql = require("mysql2");
 
+// import functions for specific db queries
+const { showDepartments } = require("./dbQueries");
+const { addDepartment } = require("./dbQueries");
+const { showRoles } = require("./dbQueries");
+const { addRole } = require("./dbQueries");
+const { showEmployees } = require("./dbQueries");
+const { addEmployee } = require("./dbQueries");
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -13,19 +22,29 @@ const menuPrompt = () => {
             type: "list",
             name: "options",
             message: "What would you like to do?",
-            choices: ["View all deptartments", "Add department", "View all roles", "Add role", "View all employees", "Add employee"]
+            choices: ["View all departments", "View all roles", "View all employees", "Add department", "Add Role", "Add employee", "Update employee role"]
         }]
     ).then((userResponse) => {
         if (userResponse.options === "View all departments") {
-            console.log("view all departments");
-        } else if(userResponse.options === "Add department") {
-            console.log("Add department");
-        } else if(userResponse.options === "Add role") {
-            console.log("Add role");
-        } else if(userResponse.options === "View all employees") {
-            console.log("View all employees");
-        } else if(userResponse.options === "Add employee") {
-            console.log("Add employee");
+            showDepartments();
+           
+        } else if (userResponse.options === "View all roles") {
+            showRoles();
+           
+        } else if (userResponse.options === "View all employees") {
+            showEmployees();
+
+        } else if (userResponse.options === "Add department") {
+            addDepartment();
+            
+        } else if (userResponse.options === "Add Role") {
+            addRole();
+    
+        } else if (userResponse.options === "Add employee") {
+            addEmployee();
+            
+        } else if (userResponse.options === "Update employee role") {
+            updateEmployee();
         }
         menuPrompt();
     })
